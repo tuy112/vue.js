7d792dd4afbe667db780b165ef31a7284e1d1aa5
Jstol <master_catch@naver.com>
2023-11-03 15:54:20 +0900

    fix: bind, seen 추가

--- vue.js
+++ vue.js
@@ -4,3 +4,17 @@ const app = new Vue({
       message: '안녕하세요 Vue!'
     }
   })
+
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: false
+    }
+  })
