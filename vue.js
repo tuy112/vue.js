5d5f36a1380942962aee98fe000ba777107ffda6
Jstol <master_catch@naver.com>
2023-11-09 13:07:04 +0900

    fix: var->const

--- vue.js
+++ vue.js
@@ -5,16 +5,16 @@ const app = new Vue({
     }
   })
 
-  var app2 = new Vue({
+  const app2 = new Vue({
     el: '#app-2',
     data: {
       message: '이 페이지는 ' + new Date() + ' 에 로드 되었습니다'
     }
   })
 
-  var app3 = new Vue({
+  const app3 = new Vue({
     el: '#app-3',
     data: {
-      seen: false
+      seen: ture
     }
   })
